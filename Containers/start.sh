68b413813e5141ca6368d61fff27b0b4fd20472a
paxerxxx <paxerxxx@gmail.com>
2026-07-07 13:35:08 +0300

    Revert "remove wget geosite.dat"
    This reverts commit 3dfdacbc4c510b61c28a8fbc4e463e22a1b9b8ba.

--- Containers/start.sh
+++ Containers/start.sh
@@ -5,15 +5,15 @@ sleep 1
 
 #TUN_IFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -E 'tun' | head -n1 | cut -d'@' -f1)
 
-#echo '30      03       *       *       *      wget https://raw.githubusercontent.com/runetfreedom/russia-blocked-geosite/release/geosite.dat -O /tmp/xray/geosite.dat && service xray restart' >> /etc/crontabs/root
-#/usr/sbin/crond
+echo '30      03       *       *       *      wget https://raw.githubusercontent.com/runetfreedom/russia-blocked-geosite/release/geosite.dat -O /tmp/xray/geosite.dat && service xray restart' >> /etc/crontabs/root
+/usr/sbin/crond
 
 
 echo "Xray preparing for launch"
 #rm -rf /tmp/xray/ && mkdir /tmp/xray/
 7z x /opt/xray/xray.7z -o/tmp/xray/ -y
 chmod 755 /tmp/xray/xray
-#wget https://raw.githubusercontent.com/runetfreedom/russia-blocked-geosite/release/geosite.dat -O /tmp/xray/geosite.dat
+wget https://raw.githubusercontent.com/runetfreedom/russia-blocked-geosite/release/geosite.dat -O /tmp/xray/geosite.dat
 
 ip rule add iif xray lookup 100
 
